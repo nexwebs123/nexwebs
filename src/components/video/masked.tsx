dbdd9464d464c94be210099169b4640cba3a7ebb
Nilav <nilavtalukdar06@gmail.com>
2025-08-30 01:03:34 +0530

    modified mask div box

--- src/components/video/masked.tsx
+++ src/components/video/masked.tsx
@@ -5,32 +5,6 @@ import MaskedDiv from "../ui/masked-div";
 export function MaksedDivBox() {
   return (
     <div className="items-between m-auto my-12 flex w-full flex-wrap justify-between gap-5">
-      <MaskedDiv maskType="type-1" size={0.45} className="my-4 hidden">
-        <video
-          className="cursor-pointer transition-all duration-300 hover:scale-105"
-          autoPlay
-          loop
-          muted
-        >
-          <source
-            src="https://videos.pexels.com/video-files/7710243/7710243-uhd_2560_1440_30fps.mp4"
-            type="video/mp4"
-          />
-        </video>
-      </MaskedDiv>
-      <MaskedDiv maskType="type-1" size={0.45} className="rotate-180 hidden">
-        <video
-          className="cursor-pointer transition-all duration-300 hover:scale-105"
-          autoPlay
-          loop
-          muted
-        >
-          <source
-            src="https://videos.pexels.com/video-files/18069803/18069803-uhd_1440_2560_24fps.mp4"
-            type="video/mp4"
-          />
-        </video>
-      </MaskedDiv>
       <MaskedDiv maskType="type-3" className="my-4">
         <video
           className="cursor-pointer transition-all duration-300 hover:scale-105"
@@ -39,12 +13,12 @@ export function MaksedDivBox() {
           muted
         >
           <source
-            src="https://videos.pexels.com/video-files/18069166/18069166-uhd_2560_1440_24fps.mp4"
+            src="https://www.pexels.com/download/video/31575746/"
             type="video/mp4"
           />
         </video>
       </MaskedDiv>
-      <MaskedDiv maskType="type-4" className="my-4">
+      {/* <MaskedDiv maskType="type-4" className="my-4">
         <video
           className="cursor-pointer transition-all duration-300 hover:scale-105"
           autoPlay
@@ -69,7 +43,7 @@ export function MaksedDivBox() {
             type="video/mp4"
           />
         </video>
-      </MaskedDiv>
+      </MaskedDiv> */}
     </div>
   );
 }
