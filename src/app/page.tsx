a8929a6e348fd3a61bf9b0b5a13d1fd8a51120da
Nilav <nilavtalukdar06@gmail.com>
2025-08-29 20:47:05 +0530

    minor home page updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import { ModeToggle } from "@/components/theme/theme-button";
-
 export default function Home() {
   return (
     <section>
-      <h1>NEXWEBS</h1>
-      <div className="p-4 my-2">
-        <ModeToggle />
+      <div>
+        <div></div>
       </div>
     </section>
   );
