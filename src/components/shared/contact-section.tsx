29d706de6fedd13e70a736f5737a80d60935ac43
Nilav <nilavtalukdar06@gmail.com>
2025-08-30 19:48:57 +0530

    added contact section

---
+++ src/components/shared/contact-section.tsx
@@ -0,0 +1,19 @@
+import { Badge } from "../theme/badge";
+
+export function ContactUs() {
+  return (
+    <section className="my-6 md:my-12 w-full">
+      <Badge title="Get In Touch" />
+      <div className="max-w-4xl mx-auto text-center">
+        <h2 className="text-3xl md:text-5xl font-semibold">
+          Let's Build Something Amazing Together
+        </h2>
+        <p className="mt-4 text-lg md:text-xl font-medium text-neutral-400">
+          Ready to transform your business with cutting-edge software solutions?
+          Our team of experts is here to help you every step of the way.
+        </p>
+      </div>
+      <div className="my-6 md:my-12"></div>
+    </section>
+  );
+}
