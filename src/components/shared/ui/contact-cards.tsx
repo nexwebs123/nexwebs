df0415120425d61845d4287dded12fc5b717b6cb
Nilav <nilavtalukdar06@gmail.com>
2025-08-30 20:04:54 +0530

    added contact cards

--- src/components/shared/ui/contact-cards.tsx
+++ src/components/shared/ui/contact-cards.tsx
@@ -1,53 +1,71 @@
+import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardAction,
   CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { MessageSquare, User, Users } from "lucide-react";
 
 export function ContactCards() {
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       <Card>
         <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-          <CardAction>Card Action</CardAction>
+          <CardTitle>
+            <Button
+              variant="secondary"
+              size="sm"
+              className="pointer-events-none"
+            >
+              <MessageSquare />
+            </Button>
+          </CardTitle>
+          <CardDescription>Live Chat</CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Card Content</p>
+          <p>Chat with our experts right now and Get your project done</p>
         </CardContent>
         <CardFooter>
-          <p>Card Footer</p>
+          <Button variant="secondary">Chat Now</Button>
         </CardFooter>
       </Card>
       <Card>
         <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-          <CardAction>Card Action</CardAction>
+          <CardTitle>
+            <Button
+              variant="secondary"
+              size="sm"
+              className="pointer-events-none"
+            >
+              <User />
+            </Button>
+          </CardTitle>
+          <CardDescription>Get In Touch</CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Card Content</p>
+          <p>Submit your personal and project details in the form</p>
         </CardContent>
         <CardFooter>
-          <p>Card Footer</p>
+          <Button variant="secondary">Submit</Button>
         </CardFooter>
       </Card>
       <Card>
         <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-          <CardAction>Card Action</CardAction>
+          <CardTitle>
+            <Button variant="secondary" size="sm">
+              <Users />
+            </Button>
+          </CardTitle>
+          <CardDescription>Meet Our Team</CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Card Content</p>
+          <p>Learn more about our experts which will get your work done</p>
         </CardContent>
         <CardFooter>
-          <p>Card Footer</p>
+          <Button variant="secondary">View Team</Button>
         </CardFooter>
       </Card>
     </section>
