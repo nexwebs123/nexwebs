de526ef0a502fa0137bef2a75c417a85a189fc72
Nilav <nilavtalukdar06@gmail.com>
2025-08-30 14:50:53 +0530

    added heading

--- src/components/shared/solutions-section.tsx
+++ src/components/shared/solutions-section.tsx
@@ -1,9 +1,34 @@
 import { Badge } from "../theme/badge";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { ArrowRight } from "lucide-react";
 
 export function Solutions() {
   return (
     <section className="mt-12 sm:mt-24 mb-12">
       <Badge title="Industry Solutions" />
+      <div className="w-full text-center">
+        <h2 className="text-3xl md:text-5xl font-semibold">
+          Tailored Solutions for Every Industry
+        </h2>
+        <p className="mt-4 text-lg md:text-xl font-medium text-neutral-400">
+          Specialized software solutions designed to meet the unique challenges
+          of your industry.
+        </p>
+      </div>
+      <div className="my-6 md:my-12"></div>
+      <div className="my-6 md:my-12"></div>
+      <div className="my-6 flex flex-col justify-center items-center text-center">
+        <p className="text-lg text-neutral-600">
+          Need a custom solution tailored to your business?
+        </p>
+        <Link href="/#contact">
+          <Button className="my-4" size="lg" variant="secondary">
+            Request Custom Quote
+            <ArrowRight />
+          </Button>
+        </Link>
+      </div>
     </section>
   );
 }
