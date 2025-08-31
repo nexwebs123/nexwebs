0189f3e30c256fae1156055682628f3140e22ac5
Nilav <nilavtalukdar06@gmail.com>
2025-08-31 11:42:33 +0530

    updated badge

--- src/components/theme/badge.tsx
+++ src/components/theme/badge.tsx
@@ -2,7 +2,7 @@ export function Badge({ title }: { title: string }) {
   return (
     <div className="w-full flex justify-center items-center">
       <div className="px-4 py-2 rounded-full flex justify-center items-center gap-x-2 border text-sm font-medium bg-orange-500/10 text-orange-600 border-orange-500/30 dark:bg-orange-500/10 dark:text-orange-400 dark:border-orange-400/30 backdrop-blur-sm mb-6 sm:mb-12 animate-pulse">
-        ✨ <p>{title}</p>
+        <p>{title}</p>
       </div>
     </div>
   );
